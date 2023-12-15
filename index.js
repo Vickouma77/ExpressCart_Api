4c360097cc6c2d39a0838a737bac79377ba8cd2a
Vickouma77 <Vickprestone20@gmail.com>
2023-12-15 14:52:59 +0300

    connecting to MongoDB Atlas using mongoose

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -6,6 +7,22 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+/* Connect to MongoDB */
+try {
+  mongoose.connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => {
+    console.log("Successfully connected to MongoDB.");
+  });
+} catch (error) {
+  console.log("Error connecting to MongoDB.");
+}
+
+
+
+/* Listen to the server */
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
